8618d3c1a131799c107e460f0b74ef6a1dbdb350
IshaGandham <113026196+IshaGandham@users.noreply.github.com>
2022-12-11 04:09:46 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+﻿
+
+gsap.to("#Video_bubble", {
+    duration: 2,
+
+    rotation: 360,
+});
+
+gsap.to("#Sound_bubble", {
+    duration: 1,
+
+    rotation: 360,
+});
+gsap.to("#Comic_bubble", {
+    duration: 1,
+
+    rotation: 360,
+});
+gsap.to("#Me_bubble", {
+    duration: 2,
+
+    rotation: 360,
+});
+gsap.to("#_30mff_bubble", {
+    duration: 1,
+
+    rotation: 360,
+});
+
